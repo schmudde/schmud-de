c99741d1875337e5ec6b2d02461abf4a9afafed1
schmudde <schmudde@beyondthefra.me>
2015-09-09 07:44:29 +0200

    Preparing for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/schmud-de-standalone.jar clojure.main -m schmud-de.web
--- project.clj
+++ project.clj
@@ -5,9 +5,13 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
+                 [environ "1.0.0"]
                  [de.ubercode.clostache/clostache "1.4.0"]]
-  :plugins [[lein-ring "0.9.6"]]
+  :plugins [[lein-ring "0.9.6"]
+            [environ/environ.lein "0.3.1"]]
+  :hooks [environ.leiningen.hooks]
   :ring {:handler schmud-de.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+  :uberjar-name "schmud-de-standalone.jar"
+  :profiles {{:production {:env {:production true}}}
+             {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                   [ring-mock "0.1.5"]]}}})
