1d50f86337410901308606345524a7a5697fc20a
schmudde <schmudde@beyondthefra.me>
2015-09-12 22:42:03 +0200

    Heroku crash fix

--- project.clj
+++ project.clj
@@ -1,17 +1,15 @@
 (defproject schmud-de "0.1.0-SNAPSHOT"
   :description "Personal homepage: schmud.de"
   :url "http://schmud.de"
-  :min-lein-version "2.0.0"
+  :license {:name "Eclipse Public License v1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-jetty-adapter "1.4.0"]
                  [environ "1.0.0"]
                  [de.ubercode.clostache/clostache "1.4.0"]]
-  :plugins [[lein-ring "0.9.6"]
-            [environ/environ.lein "0.3.1"]]
+  :min-lein-version "2.0.0"
+  :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
-;  :ring {:handler schmud-de.handler/app}
   :uberjar-name "schmud-de-standalone.jar"
-  :profiles {{:production {:env {:production true}}}
-             {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                   [ring-mock "0.1.5"]]}}})
+  :profiles {:production {:env {:production true}}})
--- src/schmud_de/handler.clj
+++ src/schmud_de/handler.clj
@@ -1,24 +1,58 @@
 (ns schmud-de.handler
-  (:require [compojure.core :refer :all]
+  (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
+            [compojure.handler :refer [site]]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [schmud-de.controllers.projects :as projects-controller]))
+            [clojure.java.io :as io]
+            [ring.adapter.jetty :as jetty]
+            [environ.core :refer [env]]))
 
-(defroutes app-routes
-  (GET "/" [] "Hello World")
+;  (:require [compojure.core :refer :all]
+;            [compojure.route :as route]
+;            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+;            [schmud-de.controllers.projects :as projects-controller]))
+
+;(defroutes app-routes
+;  (GET "/" [] "Hello World")
 ;TODO: Add this back in once the "Hello World" is working on Heroku. Needs to be called web.clj?
 ;(projects-controller/index))
-  (route/resources "/")
-  (route/not-found "Not Found"))
+;  (route/resources "/")
+;  (route/not-found "Not Found"))
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+;(def app
+;  (wrap-defaults app-routes site-defaults))
+
+;(defn splash []
+;  {:status 200
+;   :headers {"Content-Type" "text/plain"}
+;   :body (pr-str ["Hello" :from 'Heroku])})
+
+;(defn -main []
+;  ())
 
 (defn splash []
   {:status 200
    :headers {"Content-Type" "text/plain"}
    :body (pr-str ["Hello" :from 'Heroku])})
 
+(defroutes app
+;  (GET "/camel" {{input :input} :params}
+;       {:status 200
+;        :headers {"Content-Type" "text/plain"}
+;        :body (kebab/->CamelCase input)})
+;  (GET "/snake" {{input :input} :params}
+;       {:status 200
+;        :headers {"Content-Type" "text/plain"}
+;        :body (kebab/->snake_case input)})
+;  (GET "/kebab" {{input :input} :params}
+;       {:status 200
+;        :headers {"Content-Type" "text/plain"}
+;        :body (kebab/->kebab-case input)})
+  (GET "/" []
+       (splash))
+  (ANY "*" []
+       (route/not-found (slurp (io/resource "404.html")))))
+
+(def sample (env :sample "sample-string-thing"))
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
