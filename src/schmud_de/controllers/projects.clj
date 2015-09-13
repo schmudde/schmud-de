af70aed1163b165cb1a7a4e1712d34707c818ebd
schmudde <schmudde@beyondthefra.me>
2015-09-13 04:05:22 +0200

    Clostache partials

--- src/schmud_de/controllers/projects.clj
+++ src/schmud_de/controllers/projects.clj
@@ -4,10 +4,23 @@
 
 (defn read-template [template-name]
   (slurp (clojure.java.io/resource
-          (str "views/projects/" template-name ".mustache"))))
+          (str "templates/" template-name ".mustache"))))
 
-(defn render-template [template-file params]
+(defn render-template
+  "Pass in the template name (a string, sans its .mustache filename extension), the data for the template (a map), and a list of partials (keywords) corresponding to like-named template filenames."
+  [template-file params]
   (clostache/render (read-template template-file) params))
 
 (defn index []
-  (render-template "index" {:name "Schmoodes"}))
+  (clostache/render-resource
+   (str "templates/" "main.mustache")
+   {:name "Schmooodes"}
+   (reduce (fn [accum pt] ;; "pt" is the name (as a keyword) of the partial
+             (assoc accum pt (slurp (clojure.java.io/resource
+                                     (str "templates/"
+                                          (name pt)
+                                          ".mustache")))))
+             {}
+             [:header :footer])))
+
+;  (render-template "main" {:name "Schmoodes"}))
--- src/schmud_de/handler.clj
+++ src/schmud_de/handler.clj
@@ -7,37 +7,12 @@
             [environ.core :refer [env]]
             [schmud-de.controllers.projects :as projects-controller]))
 
-;  (:require [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-
-;(defroutes app-routes
-;  (GET "/" [] "Hello World")
-;TODO: Add this back in once the "Hello World" is working on Heroku. Needs to be called web.clj?
-;(projects-controller/index))
-;
-;
-
-;(def app
-;  (wrap-defaults app-routes site-defaults))
-
-
 (defn splash []
   {:status 200
    :headers {"Content-Type" "text/plain"}
    :body (pr-str ["Hello" :from 'Heroku])})
 
 (defroutes app
-;  (GET "/camel" {{input :input} :params}
-;       {:status 200
-;        :headers {"Content-Type" "text/plain"}
-;        :body (kebab/->CamelCase input)})
-;  (GET "/snake" {{input :input} :params}
-;       {:status 200
-;        :headers {"Content-Type" "text/plain"}
-;        :body (kebab/->snake_case input)})
-;  (GET "/kebab" {{input :input} :params}
-;       {:status 200
-;        :headers {"Content-Type" "text/plain"}
-;        :body (kebab/->kebab-case input)})
   (GET "/" []
        (splash))
   (GET "/projects" []
